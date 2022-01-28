75d6871a93951d52df7a58e3fc627b0468eb2ff8
Paolo Brasolin <paolo.brasolin@gmail.com>
2022-01-28 23:34:57 +0100

    feat: clean up references and write a couple specs

--- lib/jekyll/sheafy/references.rb
+++ lib/jekyll/sheafy/references.rb
@@ -2,24 +2,38 @@ module Jekyll
   module Sheafy
     module References
       RE_REF_TAG = /{%\s*ref (?<slug>.+?)\s*%}/
+      REFERRERS_KEY = "referrers"
 
-      def self.process(nodes)
-        # The structure of references is a directed graph,
-        # where source = referrer and target = referent.
+      def self.process(nodes_index)
+        adjacency_list = build_adjacency_list(nodes_index)
+        denormalize_adjacency_list!(adjacency_list, nodes_index)
+        # NOTE: topology is arbitrary so no single pass technique is possible.
+        nodes_index.values.each { |node| node.data[REFERRERS_KEY] = [] }
+        attribute_neighbors!(adjacency_list)
+      end
 
-        nodes.values.each do |source|
-          source.content.scan(RE_REF_TAG).each do |(slug)|
-            target = nodes[slug]
-            # TODO: handle missing targets
-            target.data["referrers"] ||= []
-            target.data["referrers"] << source
+      def self.scan_references(node)
+        node.content.scan(RE_REF_TAG).flatten
+      end
+
+      def self.build_adjacency_list(nodes_index)
+        nodes_index.transform_values(&method(:scan_references))
+      end
+
+      def self.denormalize_adjacency_list!(list, index)
+        # TODO: handle missing nodes
+        list.transform_keys!(&index)
+        list.values.each { |children| children.map!(&index) }
+      end
+
+      def self.attribute_neighbors!(list)
+        list.each do |referrer, referents|
+          # TODO: can referents be useful?
+          # referrer.data["referents"] = referents.uniq
+          referents.each do |referent|
+            next if referent.data[REFERRERS_KEY].include?(referrer)
+            referent.data[REFERRERS_KEY] << referrer
           end
-        end
-
-        # TODO: use a Set to avoid second pass
-        nodes.values.each do |resource|
-          resource.data["referrers"]&.uniq!
-          resource.data["referrers"] ||= []
         end
       end
     end
---
+++ spec/jekyll/sheafy/references_spec.rb
@@ -0,0 +1,60 @@
+require "jekyll/sheafy/references"
+
+describe Jekyll::Sheafy::References do
+  describe ".scan_references" do
+    it "detects nothing on empty file" do
+      node = Node.new(content: "")
+      expect(subject.scan_references(node)).to eq([])
+    end
+
+    it "detects includes mixed with text" do
+      node = Node.new(content: <<~CONTENT)
+        Lorem ipsum.
+        {% ref 0001 %}
+        Dolor sit amet.
+        {% ref 0002 %}
+        Yadda yadda yadda.
+        {% ref 0003 %}
+      CONTENT
+      expect(subject.scan_references(node)).to eq(["0001", "0002", "0003"])
+    end
+
+    it "keeps duplicates" do
+      node = Node.new(content: <<~CONTENT)
+        {% ref 0001 %}
+        {% ref 0001 %}
+      CONTENT
+      expect(subject.scan_references(node)).to eq(["0001", "0001"])
+    end
+  end
+
+  describe ".build_adjacency_list" do
+    it "builds nothing on empty index" do
+      index = {}
+      expect(subject.build_adjacency_list(index)).to eq({})
+    end
+
+    it "correctly interpretes a boring index" do
+      index = {
+        "0000" => Node.new(content: "{%ref 0001%}{%ref 0002%}{%ref 0001%}"),
+        "0001" => Node.new(content: "{%ref 0000%}{%ref 0002%}"),
+        "0002" => Node.new(content: ""),
+      }
+      expect(subject.build_adjacency_list(index)).to eq({
+        "0000" => ["0001", "0002", "0001"],
+        "0001" => ["0000", "0002"],
+        "0002" => [],
+      })
+    end
+
+    it "keeps duplicates" do
+      index = {
+        "0000" => Node.new(content: "{% ref 0001 %}\n" * 2),
+        "0001" => Node.new(content: ""),
+      }
+      expect(subject.build_adjacency_list(index)).to eq(
+        { "0000" => ["0001", "0001"], "0001" => [] }
+      )
+    end
+  end
+end

41e4151a8a97a4647ba20743e3789d16a01daddd
Paolo Brasolin <paolo.brasolin@gmail.com>
2022-01-30 18:42:37 +0100

    refactor: Taxa spec

--- spec/jekyll/sheafy/taxa_spec.rb
+++ spec/jekyll/sheafy/taxa_spec.rb
@@ -1,36 +1,37 @@
 require "jekyll/sheafy/taxa"
 
 describe Jekyll::Sheafy::Taxa do
+  let(:site) { Site.new(config: {}) }
+
   describe ".apply_taxon!" do
     it "does not explode if there's no config" do
-      site = Site.new(config: {})
       node = Node.new(site: site, data: {})
       expect { subject.apply_taxon!(node) }.
         not_to raise_error
     end
 
     it "does nothin to node w/o taxon" do
-      site = Site.new(config: { "sheafy" => { "taxa" => {
-                        "taxon_foo" => { "key" => "foo" },
-                      } } })
+      site.config = { "sheafy" => { "taxa" => {
+        "taxon_foo" => { "key" => "foo" },
+      } } }
       node = Node.new(site: site, data: { "key" => "bar" })
       expect { subject.apply_taxon!(node) }.
         not_to change { node.data }
     end
 
     it "does nothin to node w/ undefined taxon" do
-      site = Site.new(config: { "sheafy" => { "taxa" => {
-                        "taxon_foo" => { "key" => "foo" },
-                      } } })
+      site.config = { "sheafy" => { "taxa" => {
+        "taxon_foo" => { "key" => "foo" },
+      } } }
       node = Node.new(site: site, data: { "taxon" => "taxon_bar" })
       expect { subject.apply_taxon!(node) }.
         not_to change { node.data }
     end
 
     it "adds missing key" do
-      site = Site.new(config: { "sheafy" => { "taxa" => {
-                        "taxon_foo" => { "key" => "foo" },
-                      } } })
+      site.config = { "sheafy" => { "taxa" => {
+        "taxon_foo" => { "key" => "foo" },
+      } } }
       node = Node.new(site: site, data: { "taxon" => "taxon_foo" })
       expect { subject.apply_taxon!(node) }.to change { node.data }.to(
         { "taxon" => "taxon_foo", "key" => "foo" }
@@ -38,9 +39,9 @@ describe Jekyll::Sheafy::Taxa do
     end
 
     it "keeps existing key" do
-      site = Site.new(config: { "sheafy" => { "taxa" => {
-                        "taxon_foo" => { "key" => "foo" },
-                      } } })
+      site.config = { "sheafy" => { "taxa" => {
+        "taxon_foo" => { "key" => "foo" },
+      } } }
       node = Node.new(site: site, data: {
                         "taxon" => "taxon_foo", "key" => "bar",
                       })

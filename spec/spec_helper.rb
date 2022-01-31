13f1b1b919b15ae8ed374392ccb67865e4e4d2e2
Paolo Brasolin <paolo.brasolin@gmail.com>
2022-01-31 19:19:53 +0100

    fix: repair simplecov in CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,23 +1,31 @@
 require "byebug" # I might as well keep it
 
 require "simplecov"
-require "simplecov-lcov"
 
-SimpleCov::Formatter::LcovFormatter.config do |c|
-  c.report_with_single_file = true
-  c.output_directory = "coverage"
-  c.lcov_file_name = "lcov.info"
+if ENV.fetch("CC_TEST_REPORTER_ID", nil)
+  require "simplecov_json_formatter"
+else
+  require "simplecov-html"
+  require "simplecov-lcov"
+  SimpleCov::Formatter::LcovFormatter.config do |c|
+    c.report_with_single_file = true
+    c.output_directory = "coverage"
+    c.lcov_file_name = "lcov.info"
+  end
 end
 
 SimpleCov.start do
   add_filter "/spec/"
   enable_coverage :branch
-  formatter SimpleCov::Formatter::MultiFormatter.new(
-    [
-      SimpleCov::Formatter::LcovFormatter,
+
+  if ENV.fetch("CC_TEST_REPORTER_ID", nil)
+    formatter SimpleCov::Formatter::JSONFormatter
+  else
+    formatter SimpleCov::Formatter::MultiFormatter.new([
       SimpleCov::Formatter::HTMLFormatter,
-    ]
-  )
+      SimpleCov::Formatter::LcovFormatter,
+    ])
+  end
 end
 
 RSpec.configure do |config|
